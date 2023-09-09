4d6f3502bf263db2d5d7cf667784a2f25891f360
Pabitra Das <daspabitra55@gmail.com>
2023-09-09 17:08:59 +0530

    inital commit

---
+++ Enotes_spring_boot_project/src/main/java/com/becoder/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.becoder.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.becoder.entity.User;
+import com.becoder.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public User saveUser(User user) {
+		user.setRole("ROLE_USER");
+		User newUser = userRepo.save(user);
+		return newUser;
+	}
+
+	@Override
+	public boolean existEmailCheck(String email) {
+		return userRepo.existsByEmail(email);
+	}
+
+	public void removeSessionMessage() {
+		HttpSession session = ((ServletRequestAttributes) (RequestContextHolder.getRequestAttributes())).getRequest()
+				.getSession();
+
+		session.removeAttribute("msg");
+	}
+
+}
