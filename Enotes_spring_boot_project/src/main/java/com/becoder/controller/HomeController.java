4d93821e30289778e5e3a0ee8da1b8466b4e046f
Pabitra Das <daspabitra55@gmail.com>
2023-09-10 12:03:04 +0530

    login module implement

--- Enotes_spring_boot_project/src/main/java/com/becoder/controller/HomeController.java
+++ Enotes_spring_boot_project/src/main/java/com/becoder/controller/HomeController.java
@@ -46,24 +46,11 @@ public class HomeController {
 		return "redirect:/register";
 	}
 
-	@GetMapping("/login")
+	@GetMapping("/signin")
 	public String login() {
 		return "login";
 	}
 
-	@GetMapping("/addNotes")
-	public String addNotes() {
-		return "add_notes";
-	}
-
-	@GetMapping("/viewNotes")
-	public String viewNotes() {
-		return "view_notes";
-	}
-
-	@GetMapping("/editNotes")
-	public String editNotes() {
-		return "edit_notes";
-	}
+	
 
 }
